b47f778f657a78050a7a339eeb843b44e491edb7
Wes Slaughter <westonslaughter@gmail.com>
2023-01-17 12:04:10 -0500

    fix uncomented line

--- R/ms_internals.R
+++ R/ms_internals.R
@@ -2561,8 +2561,8 @@ format_citations <- function(ts_attrib, ws_attr = FALSE){
         # matches <- c(matches, mch)
     }
     
-    REAL SOLUTION: repopulate citation column with formatted citations, then do the
-    above filtering by full title, authors, date
+    # REAL SOLUTION: repopulate citation column with formatted citations, then do the
+    # above filtering by full title, authors, date
     
     #find first three words of only letters
     #find year
