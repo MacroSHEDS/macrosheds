56321698e50fe738024f17a08ae7c0f62f4e14fb
weston <westonslaughter@gmail.com>
2023-01-19 11:39:34 -0500

    ms_load_variables draft

--- R/ms_load_variables.R
+++ R/ms_load_variables.R
@@ -15,19 +15,45 @@
 #' @examples
 #' ms_vars <- ms_download_variables()
 
-ms_load_variables <- function(expanded = TRUE, verbose = TRUE){
+ms_load_variables <- function(expanded = TRUE, verbose = FALSE, type = 'all'){
 
   tryCatch(
     expr = {
-      ms_vars <- load('./data/ms_var_catalog.RData', verbose = verbose)
-
+      if(type == 'all') {
+        load('./data/ms_vars_ts.RData', verbose = verbose)
+        load('./data/ms_vars_ws_attr.RData', verbose = verbose)
+      } else if(type == 'ts') {
+        load('./data/ms_vars_ts.RData', verbose = verbose)
+      } else if(type == 'ws') {
+        load('./data/ms_vars_ws_attr.RData', verbose = verbose)
+      } else {
+        stop('type not in accepted options: "ts" for time series variable catalog "ws" for watershed attribute variable catalog, "all" for both')
+      }
     },
     error = function(e) {
-      writeLines(paste0('failed to load macrosheds variable catalog into R session'))
+      stop(paste0('failed to load macrosheds variable catalog into R session'))
     }
     )
+  # TODO: add in 'expanded' handling here
+  # calculate percentiles of var observations, remove vars in bottom 10% of number of observations
+  # this is a proxy for the 'obscurity' of a variable
+  if(expanded == FALSE) {
+    if(exists('ms_vars_ts') && is.data.frame(get('ms_vars_ts'))) {
+      ms_obs_qt = quantile(ms_vars_ts$observations, , probs = c(0.1,0.3,0.5,0.7, 0.9))
+      ms_vars_ts <- ms_vars_ts %>%
+        filter(observations > ms_obs_qt[[1]])
+    } else {
+      warning('expanded option only applies to time series variables')
+    }
+  }
 
-
-
-    return(ms_vars)
+  if(type == 'all') {
+    return(list('ms_vars_ts' = ms_vars_ts, 'ms_vars_ws' = ms_vars_ws))
+  } else if(type == 'ts') {
+    return(ms_vars_ts)
+  } else if(type == 'ws') {
+    return(ms_vars_ws)
+  } else {
+    stop('type not in accepted options: "ts" for time series variable catalog "ws" for watershed attribute variable catalog, "all" for both')
+  }
 }
