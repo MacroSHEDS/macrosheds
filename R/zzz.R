496108a84a3b1c7a624777eb9554e6715141d170
Mike Vlah <vlahm13@gmail.com>
2024-10-01 19:45:22 -0600

    added vignettes link to startup text

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
     macrosheds_dataset_link <- "https://portal.edirepository.org/nis/mapbrowse?scope=edi&identifier=1262"
-      
+
     # check if user has internet connection
     site <- "http://example.com/"
     is_online <- tryCatch(
@@ -12,21 +12,21 @@
         warning = function(w) invokeRestart("muffleWarning"),
         error = function(e) FALSE
     )
-  
+
     # if connected, check whether figshare IDs are up to date
     if(is_online){
-  
+
         id_check_url <- 'https://raw.githubusercontent.com/MacroSHEDS/macrosheds/master/data/figshare_id_check.txt'
         figshare_codes <- macrosheds::file_ids_for_r_package2 #loaded in R/sysdata2.rda
-  
-        codecol <- colnames(figshare_codes)[-1] %>% 
-            stringr::str_extract('[0-9\\.]+$') %>% 
-            as.numeric() %>% 
-            which.max() %>% 
+
+        codecol <- colnames(figshare_codes)[-1] %>%
+            stringr::str_extract('[0-9\\.]+$') %>%
+            as.numeric() %>%
+            which.max() %>%
             {. + 1}
-        
+
         test_id <- pull(figshare_codes[figshare_codes$ut == 'watershed_summaries', codecol])
-        
+
         result <- try(readLines(id_check_url, 1), silent = TRUE)
 
         if(inherits(result, 'try-error') || ! inherits(result, 'character')){
@@ -40,11 +40,12 @@
     } else {
         figshare_message <- 'Failed to verify version is up to date. No connection.'
     }
-  
+
     packageStartupMessage(
         "\nmacrosheds v", packageVersion(pkgname), ": ", figshare_message, "\n\n",
         "This package is licensed under MIT, but licensing of MacroSheds data is more complex. See \n",
         "https://docs.google.com/document/d/1CPaQ705QyoWfu6WjA4xHgQooCQ8arq3NZ8DO9tb9jZ0/edit?usp=sharing",
-        "\n\nComplete MacroSheds metadata: ", macrosheds_dataset_link
+        "\n\nComplete MacroSheds metadata: ", macrosheds_dataset_link,
+        "\nPackage vignettes: ", 'https://macrosheds.org/pages/vignettes'
     )
 }
